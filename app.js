97e3387b8b095cccc7339dbe02f4cd62f3585b3b
lomowang <frees1102@gmail.com>
2024-11-19 02:11:25 +0800

    20241119

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-import './style.css'
+import "./style.css";
+import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+gsap.registerPlugin(Flip, GSDevTools);
+
+const preloaderBackground = document.querySelector("preloader_background");
+
+const preloaderText = document.querySelector(".preloader_text");
+
+const master = gsap.timeline();
+
+const setIntervalStates = () => {
+  gsap.set(preloaderText, {
+    yPercent: 100,
+  });
+};
+
+master.add(setIntervalStates());
